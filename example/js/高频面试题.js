f29d1b4a53f40df015c14c590a0b30805905b5d2
dengting <491273896@qq.com>
2022-09-23 09:41:07 +0800

    feat: 文档补充

---
+++ example/js/高频面试题.js
@@ -0,0 +1,39 @@
+// 1. ['1', '2', '3'].map(parseInt) what & why ?
+
+// let a1 = ["1", "2", "3"].map(parseInt);
+// console.log("111111-a1", a1);
+// // [0, NaN, NaN]
+
+
+// var b = 10;
+// (function () {
+//     b = 20;
+//     console.log("b", b)
+// })()
+
+
+// 如何让  if( a == 1 && a == 2 && a == 3 ) {  console.log(1); } 打印1
+
+// 方法1
+// var a = {
+//     i: 1,
+//     toString() {
+//         return a.i++;
+//     }
+// }
+
+// // 方法2
+// var a = [1,2,3]
+// a.toString = a.shift;
+
+// if (a == 1 && a == 2 && a == 3) {
+//     console.log(1);
+// }
+
+
+// 4.
+var a = {n: 1}; 
+var b = a;
+a.x = a = {n: 2};
+console.log(a) 
+console.log(b)
