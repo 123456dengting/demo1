d6b16c7540454130e201b14a48bf348281128943
dengting <491273896@qq.com>
2022-09-26 11:06:39 +0800

    feat： 补充资料文档

--- example/js/event_loop.js
+++ example/js/event_loop.js
@@ -1,15 +1,26 @@
+
+
+// setTimeout(() => {
+//     console.log("setTimeout1")
+//     Promise.resolve().then(() => {
+//         console.log("Promise1");
+//     })
+// })
+
+
+
+// setTimeout(() => {
+//     console.log("setTimeout2")
+//     Promise.resolve().then(() => {
+//         console.log("Promise2");
+//     })
+// })
+
+console.log("1111111");
+console.log("11112221");
 setTimeout(() => {
-    console.log("setTimeout1")
-    Promise.resolve().then(() => {
-        console.log("Promise1");
-    })
-})
-
-
-
-setTimeout(() => {
-    console.log("setTimeout2")
-    Promise.resolve().then(() => {
-        console.log("Promise2");
-    })
-})
+    console.log("setTimeout0")
+}, 0)
+setImmediate(() => {
+    console.log("setImmediate0")
+}, 0)
